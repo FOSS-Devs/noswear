f7a264db61ec8efd2de1f27d5647d3e739a23733
Sven <68018116+SKBotNL@users.noreply.github.com>
2021-07-23 17:00:29 +0200

    rename to check

--- noswear/checker.py
+++ noswear/checker.py
@@ -5,7 +5,7 @@ _ROOT = os.path.abspath(os.path.dirname(__file__))
 def get_data(path):
     return os.path.join(_ROOT, path)
 
-def checker(sentence):
+def check(sentence):
     with open(get_data('wordlist.txt'), "r") as words:
         lines = words.readlines()
     wordlist = [l.replace("\n", "") for l in lines]
@@ -18,10 +18,11 @@ def checker(sentence):
     e = ["e", "*", "3"]
     s = ["s", "$", "5"]
     t = ["t", "7"]
-    spec_char = {"!": "i", "@": "a", "$": "s"}
+    spec_char = {"@": "a", "1": "i", "!": "i", "0": "o", "1": "l", "3": "e", "$": "s", "5": "s"}
     sentence = sentence.lower()
     for attr, value in spec_char.items():
         sentence = sentence.replace(attr, value)
+
     for j in wordlist:
         if j in sentence:
             return True
