4a86cb00f519d2b69627c767b482d8b7f4a589ee
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-07-24 21:43:59 +0800

    Haven't added difflib as requirement yet.

--- noswear/checker.py
+++ noswear/checker.py
@@ -1,31 +1,52 @@
 # Copyright (c) 2021 FOSS-Devs
 # See LICENSE in the project root for license information.
-import os
+import os, difflib
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 def get_data(path):
     return os.path.join(_ROOT, path)
 
-def check(sentence):
-    with open(get_data('wordlist.txt'), "r") as words:
+def check(sentence, similarity: float = 0.75, lib: str = "wordlist.txt"):
+    with open(get_data(lib), "r") as words:
         lines = words.readlines()
     wordlist = [l.replace("\n", "") for l in lines]
-    a = ["a", "@", "*"]
-    i = ["i", "*", "1", "!"]
-    o = ["o", "*", "@", "0"]
-    u = ["u", "*"]
-    v = ["v", "*"]
-    l = ["l", "1"]
-    e = ["e", "*", "3"]
-    s = ["s", "$", "5"]
-    t = ["t", "7"]
+    #a = ["a", "@", "*"]
+    #i = ["i", "*", "1", "!"]
+    #o = ["o", "*", "@", "0"]
+    #u = ["u", "*"]
+    #v = ["v", "*"]
+    #l = ["l", "1"]
+    #e = ["e", "*", "3"]
+    #s = ["s", "$", "5"]
+    #t = ["t", "7"]
     spec_char = {"@": "a", "1": "i", "!": "i", "0": "o", "1": "l", "3": "e", "$": "s", "5": "s"}
     sentence = sentence.lower()
     for attr, value in spec_char.items():
         sentence = sentence.replace(attr, value)
-
     for j in wordlist:
         if j in sentence:
+            return True 
+    if " " in sentence:
+        sentence = " ".join(sentence.split())
+        for i in sentence.split(" "):
+            i = ''.join(filter(str.isalpha, i))
+            if not diffcheck(sentence, similarity, lib):
+                pass
+            else:
+                return True
+    else:
+        if diffcheck(sentence, similarity, lib):
             return True
     return False
+
+def diffcheck(sentence, similarity: float = 0.75, lib: str = "wordlist.txt"):
+    with open(get_data(lib), "r") as words:
+        lines = words.readlines()
+    wordlist = [l.replace("\n", "") for l in lines]
+    for keyword in wordlist:
+        score = difflib.SequenceMatcher(None, sentence.lower(), keyword).quick_ratio()
+        if score >= similarity:
+            return True
+    return False
+    
