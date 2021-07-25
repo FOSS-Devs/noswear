835759e4058af91b46907b8963b94917fc2cee13
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-07-25 18:47:04 +0800

    Change.

--- noswear/__init__.py
+++ noswear/__init__.py
@@ -1,3 +1,3 @@
 # Copyright (c) 2021 FOSS-Devs
 # See LICENSE in the project root for license information.
-from noswear.checker import Noswear
+from noswear.checker import noswear
--- noswear/checker.py
+++ noswear/checker.py
@@ -3,17 +3,22 @@
 import os
 import difflib
 
-class Noswear():
+class noswear():
     path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data\\wordlist.txt'))
 
-    def __init__(self):
-        pass
+    def __init__(self, string, similarity: float = 0.76, path = path):
+        self.string = string
+        self.similarity = similarity
+        self.path = path
+        self.result = False
+        self._check()
 
-    def check(self, string, similarity: float = 0.76, path = path):
-        with open(f"{path}", "r") as words:
+    def _check(self):#, string, similarity: float = 0.76, path = path):
+        self.result = False
+        with open(f"{self.path}", "r") as words:
             badwords = words.read().splitlines()
         spec_char = {"@": "a", "1": "i", "!": "i", "0": "o", "1": "l", "3": "e", "$": "s", "5": "s", "4": "a"}
-        string = string.lower()
+        string = self.string.lower()
         for attr, value in spec_char.items():
             string = string.replace(attr, value)
         if ' ' in string:
@@ -21,30 +26,28 @@ class Noswear():
             for word in string.split(' '):
                 word = ''.join(filter(str.isalpha, word))
                 for badword in badwords:
-                    if self._checker(word, badword, similarity):
-                        return True
+                    if self._checker(word, badword, self.similarity):
+                        self.result = True
+                        return self.result
         else:
             for badword in badwords:
-                if self._checker(string, badword, similarity):
-                    return True
-        return False
+                if self._checker(string, badword, self.similarity):
+                    self.result = True
+                    return self.result
+        return self.result
 
     def _diffcheck(self, word, badword, similarity: float):
         score = difflib.SequenceMatcher(None, word, badword, autojunk=False).ratio()
         if score >= similarity:
-            #print(f"Word: '{word}' Badword: '{badword}' Score: '{score}'")
             return True
         return False
 
     def _checker(self, string, badword, similarity: float):
         if badword == string:
-            #print(f"Word: '{badword}' Badword: '{badword}' 'location 1'")
             return True 
         elif len(string) == len(badword) and self._diffcheck(string, badword, similarity):
-            #print("'location 2'")
             return True
         elif len(string) >= 6 and len(badword) > 3:
             if badword in string or string in badword:
-                #print(f"Word: '{string}' Badword: '{badword}' 'location 3'")
                 return True
         return False
