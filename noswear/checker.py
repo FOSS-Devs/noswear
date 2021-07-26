90315ba576d1fc664537addd79b3694d50c2f1ec
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-07-26 13:56:28 +0800

    Enhanced.

--- noswear/checker.py
+++ noswear/checker.py
@@ -2,6 +2,7 @@
 # See LICENSE in the project root for license information.
 import os
 import difflib
+import textwrap
 
 class noswear():
     badlibpath = os.path.join(os.path.abspath(os.path.dirname(os.path.abspath(__file__))), "wordlist.txt")
@@ -9,7 +10,10 @@ class noswear():
     
     def __init__(self, string, sensitivity: float = 0.73, badlib = badlibpath, whitelist = whitelist):
         self.string = string
-        self.sensitivity = sensitivity
+        if sensitivity < 0 or sensitivity > 100:
+            raise ValueError
+        self.sensitivity = ((sensitivity * 0.003) / 85)
+        self.score = None
         self.badlib = badlib
         self.whitelist = whitelist
         self.getresult = False
@@ -41,6 +45,15 @@ class noswear():
                     if self._checker(string, badword, self.sensitivity):
                         self.getresult = True
                         return self.getresult
+        elif ' ' not in string in string and len(string) > 8:
+            string = ''.join(filter(str.isalpha, string))
+            string = textwrap.wrap(string, 5)
+            for badword in badwords:
+                for word in string:
+                    if not string in normal_words:
+                        if self._checker(word, badword, self.sensitivity):
+                            self.getresult = True
+                            return self.getresult
         else:
             string = ''.join(filter(str.isalpha, string))
             for badword in badwords:
@@ -56,18 +69,21 @@ class noswear():
         if len(badword) <= oversize and len(badword) >= undersize:
             score = difflib.SequenceMatcher(None, word, badword, autojunk=False).ratio()
             if score >= sensitivity:
-                self.fullresult = {"method": 2, "badword": badword, "detected": word, "score": score}
+                self.score = score
                 return True
         return False
 
     def _checker(self, string, badword, sensitivity: float):
+        x = len(string)
+        sensitivity = (sensitivity) * (x ** 2) + 0.002 * x + 0.5
         if badword == string:
             self.fullresult = {"method": 1, "badword": badword, "detected": string}
             return True 
         elif self._diffcheck(string, badword, sensitivity):
+            self.fullresult = {"method": 2, "badword": badword, "detected": string, "score": self.score}
             return True
         elif len(string) > 3 and len(badword) > 3:
-            if badword in string:
-                self.fullresult = {"method": 3, "badword": badword, "detected": string}
+            if badword in string or self._diffcheck(string, badword, sensitivity):
+                self.fullresult = {"method": 3, "badword": badword, "detected": string, "score": self.score}
                 return True
         return False
