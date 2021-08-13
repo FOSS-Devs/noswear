2c184edb338a4f123fecc106d78936c9245f49ae
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-08-13 18:22:41 +0800

    Change.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    data_files=[('noswear', ['noswear/wordlist.txt']), ('noswear', ['noswear/clean.json']) ],
+    data_files=[('noswear', ['noswear/data/wordlist.txt']), ('noswear', ['noswear/data/clean.json']) ],
     package_data={
         'badwords': ['noswear/wordlist.txt'],
         'cleanword': ['noswear/clean.json']
